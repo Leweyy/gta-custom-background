0fefd93831f53daea098e57ffd137181d61d9557
Leweyy <36248533+Leweyy@users.noreply.github.com>
2018-11-30 03:00:25 +0000

    Update custom-background.js

--- custom-background.js
+++ custom-background.js
@@ -1,43 +1,41 @@
 //on load
 let backgroundDiv = document.getElementsByClassName('background');
 
-$(backgroundDiv).ready(function(){
+//check for cookie
+let bgUrl = window.getCookie('custom-background') ;
 
-  //check for cookie
-  let bgUrl = window.getCookie('custom-background');
+// if found set background to cookie value
+if (bgUrl) {
+    setBackground(bgUrl);
+}
 
-  // if found set background to cookie value
-  if (bgUrl) {
-      setBackground(bgUrl);
-  }
+// create change background button
+let cb = document.createElement("Button");
+cb.innerHTML = "Change Background";
+cb.style = "top:0;right:0;position:absolute;z-index: 9999";
+document.body.appendChild(cb);
 
-  // create change background button
-  var cb = document.createElement("Button");
-  cb.innerHTML = "Change Background";
-  cb.style = "top:0;right:0;position:absolute;z-index: 9999";
-  document.body.appendChild(cb);
+cb.onclick = function() {
 
-  cb.onclick = function() {
-  
-      let bgUrl = window.prompt("Enter url of background image", "");
-  
-      if (!bgUrl) return;
-      if (!isValidUrl(bgUrl)) return alert("Not a valid url");
+    let bgUrl = window.prompt("Enter url of background image", "");
 
-      console.log("Url was valid setting cookie and changing background");
-      window.setCookie('custom-background', bgUrl);
-      setBackground(bgUrl);
-      
-  };
-  
-});
+    if (!bgUrl) return;
+    if (!isValidUrl(bgUrl)) return alert("Not a valid url");
+
+    console.log("Url was valid setting cookie and changing background");
+    window.setCookie('custom-background', bgUrl);
+    setBackground(bgUrl);
+
+};
 
 function getCookie(name) {
-	return document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)')[2];
+    let value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    if (!value) return;
+    return value[2]
 }
 
 function setCookie(name, value) {
-    document.cookie = `${name}=${value}; expires=01/01/2030; path='/';`;
+    document.cookie = `${name}=${value}; expires=Wed, 1 Jan 2020 20:47:11 UTC; path='/';`;
 }
 
 function setBackground (url) {
@@ -46,13 +44,13 @@ function setBackground (url) {
 }
 
 function isValidUrl (url) {
-  
-  let isUrlReg = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm.exec(url);
 
-  if (!isUrlReg) {
-    return false
-  }
+    let isUrlReg = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm.exec(url);
 
-  return true
-  
+    if (!isUrlReg) {
+        return false
+    }
+
+    return true
+
 }
